0ea91dca9c31fc1603a6e9c0388572daca96f8be
OttoPD <139186801+OttoPD@users.noreply.github.com>
2025-02-06 00:06:25 +0700

    Update Roombooking.jsx

--- src/pages/Roombooking.jsx
+++ src/pages/Roombooking.jsx
@@ -6,12 +6,7 @@ import BuildingAndFloorSelector from "../components/BuildingAndFloorSelector";
 const Roombooking = () => {
 
   return (
-<<<<<<< HEAD
 <div>
-=======
-    <div>
- <div>
->>>>>>> 8e5dbea3c7be53b9450b08a65404660a8f2a8778
       <div className="bg-blue-600 w-full h-[125px]"><Navbar /></div>
       <div className="p-6 bg-white min-h-screen">
       <header className="py-4">
@@ -29,7 +24,6 @@ const Roombooking = () => {
               <p className="text-xs">{["MON", "TUE", "WED", "THU", "FRI", "SAT" , "SUN"][index]}</p>
             </div>
           ))}
-<<<<<<< HEAD
         </div>
       </div>
 
@@ -56,8 +50,6 @@ const Roombooking = () => {
               <input type="text" placeholder="User ID" className="w-full border rounded-2xl px-2 py-2 mt-2 text-black bg-white" />
             </div>
           </div>
-=======
->>>>>>> 8e5dbea3c7be53b9450b08a65404660a8f2a8778
         </div>
               
         <div className="bg-blue-600 text-white p-6 rounded-lg flex flex-col h-[500px] relative">
@@ -69,7 +61,6 @@ const Roombooking = () => {
         <p className="text-white-200 text-sm font-bold">{time}</p>
         <div className="w-full h-12 bg-gray-200 rounded-xl"></div> {/* กล่องเวลา */}
       </div>
-<<<<<<< HEAD
     ))}
   </div>
 
@@ -84,69 +75,6 @@ const Roombooking = () => {
       </div>
     </div>
     <Footer/>
-=======
-
-      <div className="grid grid-cols-2 gap-6 mt-6">
-        <div className="bg-blue-600 text-white p-6 rounded-lg">
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label className="block font-bold mb-1">Start Date:</label>
-              <input type="date" className="w-full border rounded-2xl px-2 py-5 text-black bg-white" />
-            </div>
-            <div>
-              <label className="block font-bold mb-1">End Date:</label>
-              <input type="date" className="w-full border rounded-2xl px-2 py-5 text-black bg-white" />
-            </div>
-          </div>
-
-          <div className="mt-4">
-            <label className="block font-bold mb-3 text-3xl">Select Building:</label>
-            <div className="flex space-x-2 ">
-              {["LX", "CB 2", "SIT"].map((building, index) => (
-                <button
-                  key={index}
-                  className="bg-gray-300 px-10 py-2 rounded-3xl text-black hover:bg-gray-400"
-                >
-                  {building}
-                </button>
-              ))}
-            </div>
-          </div>
-
-          <div className="grid grid-cols-2 gap-4 mt-4">
-            <div>
-              <label className="block font-bold mb-1 text-3xl">Select Floor:</label>
-              <input type="text" className="w-full border rounded-2xl px-2 py-2 text-black bg-white" />
-            </div>
-            <div>
-              <label className="block font-bold mb-1 text-3xl">User:</label>
-              <input type="text" placeholder="Name" className="w-full border rounded-2xl px-2 py-2 text-black bg-white" />
-              <input type="text" placeholder="User ID" className="w-full border rounded-2xl px-2 py-2 mt-2 text-black bg-white" />
-            </div>
-          </div>
-          
-
-          <div className="mt-5 flex items-center">
-            <input type="checkbox" id="saved" className="mr-3 " />
-            <label htmlFor="saved">Saved</label>
-          </div>
-        </div>
-
-        <div className="bg-blue-600 text-white p-6 rounded-lg">
-          <p className="text-lg font-bold mb-4">All Day</p>
-          {["8.00", "9.00", "10.00", "11.00"].map((time, i) => (
-            <div key={i} className="mb-2">
-              <p className="text-white-200 text-sm font-bold">{time}</p>
-              <div className="w-full h-45 bg-gray-200 rounded-4xl"></div>
-            </div>
-          ))}
-          <button className="w-full bg-gray-200 text-black p-2 rounded-2xl mt-4 hover:bg-gray-400">Book Now</button>
-        </div>
-      </div>
-    </div>
-    </div>
-      <Footer />
->>>>>>> 8e5dbea3c7be53b9450b08a65404660a8f2a8778
     </div>
   );
 }
